37645f3a3ac062e7c2d26815b82706f591082336
olivier-boulanger <olivier.webdev@gmail.com>
2020-02-05 09:24:39 +0100

    commit test

---
+++ src/morpion/Main.java
@@ -0,0 +1,61 @@
+package morpion;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Morpion morp = new Morpion(); // création de l'objet morp à partir de la classse Morpion
+        morp.initialise(); // on initialise le tableau de la nouvelle partie
+        System.out.println(morp.affiche()); // on affiche la grille
+        Scanner sc = new Scanner(System.in); // on deigne une variable pour récupérer l'entrée clavier
+        int x; // variable pour le choix vertical
+        int y; // variable pour le choix horizontal
+        char resp; // variable pour la réponse finale si l'on veut refaire une partie
+        do { // boucle entourant le jeu complet que l'on parcourt tant que les joueurs veulent refaire une partie
+            do { // boucle tant que la partie n'est pas fini
+                do { // boucle pour le premier joueur
+                    System.out.println("JOUEUR 1");
+                    System.out.println("Quel valeur pour x ?"); // on demande les valeurs de la case souhaitée
+                    x = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("Quel valeur pour y ?");
+                    y = sc.nextInt();
+                    sc.nextLine();
+                    morp.coupJoueur1(x, y); // on appelle la méthode coupJoueur avec les valeurs saisies pour attribuer le coup ou faire rejouer (voir la méthode)
+                } while (morp.replay); // on boucle sur le premier joueur si la case choisie est déjà prise ou non comprise dans la grille de jeu
+                System.out.println(morp.affiche()); // on fait appelle à la méthode pour afficher la grille modifiée après le coup joué
+                morp.winGame(1); // on vérifie avec la méthode winGame si le joueur a gagné ou si la partie continue
+                if (morp.over) { // si la partie est terminée on quitte la boucle (ligne 51)
+                    break;
+                }
+                if (morp.endGame == true) { // on quitte la boucle si le nombres de coups joués est à 9 (maximum au morpion
+                    break;
+                }
+                do {
+                    System.out.println("JOUEUR 2");
+                    System.out.println("Quel valeur pour x ?");
+                    x = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("Quel valeur pour y ?");
+                    y = sc.nextInt();
+                    sc.nextLine();
+                    morp.coupJoueur2(x, y);
+                } while (morp.replay);
+                System.out.println(morp.affiche());
+                morp.winGame(2);
+                if (morp.over) {
+                    break;
+                }
+            } while (morp.endGame == false); // on redemande aux joueurs un choix tant que la partie n'est pas terminée
+            if (morp.over == false) { // si la partie est terminée (9 coups) et qu'il n'y a pas eu de systèmes gagnants
+                System.out.println("MATCH NUL"); 
+            }
+            System.out.println("FIN DE LA PARTIE"); // message de fin de partie
+            System.out.println("Voulez-vous rejouer ?");
+            resp = sc.nextLine().toUpperCase().charAt(0); // on récupére la premiere lettre saisie mis en majuscule des joueurs dans la variable resp
+        } while (resp == 'O'); // tant que les joueurs saisissent o ou O, la partie recommence
+        System.out.println("DOMMAGE ! Au revoir !"); // sinon fin du programme
+    }
+
+}
---
+++ src/morpion/Morpion.java
@@ -0,0 +1,96 @@
+package morpion;
+
+public class Morpion {
+
+    public int[][] grille; // déclaration de ma grille de jeu
+    public int VIDE = 0; // variable pour les cases pas encore joués
+    public int ROND = 1; // variable pour les cases jouées par le joueur 1
+    public int CROIX = 2; // variable pour les cases jouées par le joueur 2
+    public String str = ""; // variable d'affichage de ma grille
+    public int compteur = 0; // variable me permettant de bien positionner la grille en calculant le modulo de 3
+    public int coup = 0; // variable pour comptabiliser le nombre de coups joués
+    public boolean replay; // variable pour faire rejouer le joueur en cas de case déjà prise ou d'indices non compris dans le jeu
+    public boolean endGame = false; // variable pour notifier la fin de la partie si 9 coups joués
+    public boolean over = false; // variable pour déterminer si la partie est finie
+
+    public void initialise() { // methode d'initialisation de la grille en attribuant la valeur VIDE à tous les éléments du tableau
+        grille = new int[3][3];
+        for (int i = 0; i < grille.length; i++) {
+            for (int j = 0; j < grille[i].length; j++) {
+                grille[i][j] = VIDE;
+            }
+        }
+    }
+
+    public String affiche() { // methode qui affiche la grille après chaque tour 
+        for (int i = 0; i < grille.length; i++) {
+            for (int j = 0; j < grille[i].length; j++) {
+                compteur++;
+                if (compteur % 3 == 0) { // je fais un retour chariot et une tabulation pour bien mettre en place ma grille de morpion
+                    str += grille[i][j] + " \n\n";
+                } else {
+                    str += grille[i][j] + " \t";
+                }
+            }
+        }
+        System.out.println("COUP N° " + coup + "\n"); // j'affiche le nombre de coups joués
+        if (coup == 9) { // condition sur le fin de partie 
+            endGame = true;
+        } else {
+            endGame = false;
+        }
+        return str;
+    }
+
+    public void coupJoueur1(int x, int y) { // methode pour vérifier et ettribuer une case pour le joueur 1
+        if ((x < 0 || x > 2) || (y < 0 || y > 2)) { // les indices doivent être entre 0 et 2
+            System.out.println("Veuillez choisir un entier entre 0 et 2"); // sinon j'en informe le joueur
+            str = ""; // réinitialisation de l'affichage de la grille
+            replay = true; // attribution au boolean pour que le joueur rejoue
+        } else if (grille[x][y] != VIDE) { // si la case a déjà été joué
+            System.out.println("Cette case est déjà jouée !");
+            str = "";// réinitialisation de l'affichage de la grille
+            replay = true;// attribution au boolean pour que le joueur rejoue
+        } else { // sinon si la case est disponible
+            grille[x][y] = ROND; // on attribue la valeur corrspondant au joueur 1
+            str = "";// réinitialisation de l'affichage de la grille
+            coup++; // on incremente le nombre de coups joués au total
+            replay = false; // on peut passer au joueur suivant
+        }
+    }
+
+    public void coupJoueur2(int x, int y) {
+        if ((x < 0 || x > 2) || (y < 0 || y > 2)) {
+            System.out.println("Veuillez choisir un entier entre 0 et 2");
+            str = "";
+            replay = true;
+        } else if (grille[x][y] != VIDE) {
+            System.out.println("Cette case est déjà jouée !");
+            str = "";
+            replay = true;
+        } else {
+            grille[x][y] = CROIX;
+            str = "";
+            coup++;
+            replay = false;
+        }
+    }
+    
+    public void winGame(int gamer){ // methode de verification de victoires
+        if (       (grille[0][0] == gamer && grille[0][1] == gamer && grille[0][2] == gamer) // horizontale 1
+                || (grille[1][0] == gamer && grille[1][1] == gamer && grille[1][2] == gamer) // horizontale 2
+                || (grille[2][0] == gamer && grille[2][1] == gamer && grille[2][2] == gamer) // horizontale 3
+                || (grille[0][0] == gamer && grille[1][0] == gamer && grille[2][0] == gamer) // verticale 1
+                || (grille[1][0] == gamer && grille[1][1] == gamer && grille[1][2] == gamer) // verticale 2
+                || (grille[2][0] == gamer && grille[2][1] == gamer && grille[2][2] == gamer) // verticale 3
+                || (grille[0][0] == gamer && grille[1][1] == gamer && grille[2][2] == gamer) // diagonale 1
+                || (grille[0][2] == gamer && grille[1][1] == gamer && grille[2][0] == gamer) // diagonale 2
+                ){
+            System.out.println("Le Joueur " + gamer + " a gagné");
+            over = true; // variable qui désigne la fin de la partie
+        } else {
+            over = false; // la partie continue, il n'y a aucun gagnant
+        }
+    }
+
+}
